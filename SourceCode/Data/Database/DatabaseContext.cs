e641bdb1acc10be17f52274368bf8660dfd1e421
Bernd Van Belle <Bernd.VanBelle@allphi.eu>
2021-09-17 15:47:55 +0200

    Week 1 - Second Push

--- SourceCode/Data/Database/DatabaseContext.cs
+++ SourceCode/Data/Database/DatabaseContext.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FleedManagement.Api.Data.Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FleedManagement.Api.Data.Database
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<ReadTankkaart> Tankkaarten { get; set; }
+        public DbSet<ReadChauffeur> Chauffeurs { get; set; }
+        public DbSet<ReadVoertuig> Voertuigen { get; set; }
+        public DbSet<ReadHerstelling> Herstellingen { get; set; }
+        public DbSet<ReadOnderhoudsbeurt> Onderhoudsbeurten { get; set; }
+
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-
         }
     }
 }
